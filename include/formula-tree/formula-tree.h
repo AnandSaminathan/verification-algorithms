e54c3f6d217e12c30f6dcd6b7454932835977de9
Anand <anand.saminathan1.1@gmail.com>
2020-03-25 20:32:54 +0530

    Modified formula tree.

--- include/formula-tree/formula-tree.h
+++ include/formula-tree/formula-tree.h
@@ -1,18 +1,18 @@
-#pragma once
+#ifndef formula_tree
+#define formula_tree
 
 #include <string>
-#include <vector>
 #include <map>
+#include <memory>
+#include <algorithm>
 #include <assert.h>
 
 class FormulaNode {
   public:
-    FormulaNode(std::string content, std::vector<FormulaNode*> children) {
-      this->content = content;
-      this->childrenCount = children.size();
-      this->children = children;
-      this->val = false;
-    }
+    FormulaNode(std::string& content, std::shared_ptr<std::shared_ptr<FormulaNode>[]>& children, int childrenCount)
+    : content(content),
+      children(children),
+      childrenCount(childrenCount) { }
 
     FormulaNode(const FormulaNode& other) {
       (*this) = other;
@@ -27,18 +27,18 @@ class FormulaNode {
 
     inline int getChildrenCount() { return childrenCount; }
     inline FormulaNode getChild(int child) { assertChild(child); return (*children[child]); }
-    inline FormulaNode* getPointerToChild(int child) { assertChild(child); return (children[child]); }
+    inline std::shared_ptr<FormulaNode> getPointerToChild(int child) { assertChild(child); return (children[child]); }
     
     inline void setType(std::string type) { contentType = type; }
     inline void setContent(std::string content) { this->content = content; }
     inline void toggleIsVar() { val = true; }
 
   private:
-    bool val;
+    bool val{false};
     int childrenCount;
     std::string content;
     std::string contentType;
-    std::vector<FormulaNode*> children;
+    std::shared_ptr<std::shared_ptr<FormulaNode>[]> children;
 
     inline void assertChild(int child) { assert(child >= 0 && child < childrenCount); }
 };
@@ -46,8 +46,8 @@ class FormulaNode {
 
 class FormulaTree {
   public:
-    FormulaTree(std::string formula) {
-      this->formula = formula;
+    FormulaTree(std::string formula)
+    : formula(formula) {
       this->constructTree();
     }
 
@@ -55,17 +55,18 @@ class FormulaTree {
 
     inline std::string getFormula() { return toString((*root)); }
     inline FormulaNode getRoot() { return (*root); }
-    inline FormulaNode* getPointerToRoot() { return (root); }
+    inline std::shared_ptr<FormulaNode> getPointerToRoot() { return (root); }
     inline FormulaNode getNNFRoot() { return (*nnfRoot); }
     void substitute(std::map<std::string, std::string>&);
 
   private:
-    FormulaNode *root;
-    FormulaNode *nnfRoot;
+    std::shared_ptr<FormulaNode> root;
+    std::shared_ptr<FormulaNode> nnfRoot;
     std::string formula;
     void constructTree();
     void constructNNF();
     std::string toString(FormulaNode);
 };
 
+#endif
 
