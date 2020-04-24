fdaa9378324f72a2b41d82b8d461a01e38ed6b0e
Anand <anand.saminathan1.1@gmail.com>
2020-04-24 16:37:01 +0530

    Included cnf class and ic3 class

---
+++ include/verification-algorithms/common/cnf.hpp
@@ -0,0 +1,30 @@
+#pragma once
+
+#include "z3-solver.hpp"
+
+class CNF {
+  public:
+
+    CNF() : formula(ctx) { 
+      formula = ctx.bool_val(true); 
+    }
+    
+    CNF(std::string formula) : formula(ctx) {
+      formula = ctx.bool_val(true);
+    }
+
+    inline z3::expr operator()() { return formula; }
+    
+    inline void addClause(z3::expr clause) { clauses.push_back(clause); formula = (formula && clause);}
+    
+    inline z3::expr getCNF() { return formula; }
+    inline std::vector<z3::expr> getClauses() { return clauses; }
+
+
+  private:
+
+    std::vector<z3::expr> clauses;
+    z3::expr formula;
+
+};
+
---
+++ include/verification-algorithms/ic3/ic3.hpp
@@ -0,0 +1,45 @@
+#pragma once
+
+#include <vector>
+
+#include "verification-algorithms/common/verifier.hpp"
+#include "verification-algorithms/common/symbol.hpp"
+
+class IC3 : public Verifier {
+  public:
+
+    IC3(std::vector<Symbol> symbols, std::string I, std::string T) : T(ctx),
+      P(ctx),
+      x(ctx),
+      next_x(ctx),
+      trace(ctx) {
+      this->declare();
+      this->I = CNF(I);
+      this->T = stringToZ3(T);
+    }
+    
+    bool check(std::string) override;
+
+    inline z3::expr getI() { return I(); }
+    inline z3::expr getT() { return T; }
+    inline z3::expr getP() { return P; }
+
+    inline int getLength() override { return stoppedAt; }
+    inline z3::model getTrace() override { assert(result == false); return trace;  }
+
+  private:
+    
+    CNF I;
+    z3::expr T; 
+    z3::expr P;
+    z3::expr_vector x
+    z3::expr_vector next_x;
+
+    z3::model trace;
+
+    int stoppedAt;
+    bool result;
+    std::vector<Symbol> symbols;
+};
+
+
